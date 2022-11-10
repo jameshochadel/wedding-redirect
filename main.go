31f87ddc12e9a60fb10e02699d891faba7f4ce5f
James Hochadel <james.hochadel@hey.com>
2022-11-09 20:12:02 -0500

    Make Render health checks happy

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	http.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
 	http.ListenAndServe("", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Location", "https://sayi.do/lizzyandjames")
 		w.WriteHeader(301)
